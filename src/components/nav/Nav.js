b20aec8f148e2e6e74b24f2c18ff53eee7e0fc1f
Jay Hwang <jayhwang0121@gmail.com>
2018-08-20 20:25:21 -0700

    feat(Nav.js): import & display NavMenu & Navfilter components

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,24 +1,28 @@
 // React
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-const Nav = () => (
-  <nav className="nav">
-    <content>
-      <div className="logo">
-        <Link to="/">
-          {/* TODO: Get svg file for logo and inject here */}
-          <img
-            src="https://res.cloudinary.com/ddgtwtbre/image/upload/v1531042766/hundredx-logo_knkvo1.png"
-          />
-        </Link>
-      </div>
-      {/* <div className="user-session">
-        <button>Sign Up</button>
-        <button>Log in</button>
-      </div> */}
-    </content>
-  </nav>
-);
+// Components
+import Logo from './Logo';
+import NavFilter from './NavFilter';
+import NavMenu from './NavMenu';
+
+class Nav extends React.Component {
+  // TODO: connect with steem user state
+  state = {
+    isLoggedIn: true,
+  };
+
+  render() {
+    return (
+      <nav className="nav">
+        <content>
+          <Logo />
+          <NavFilter />
+          <NavMenu isLoggedIn={ this.state.isLoggedIn } />
+        </content>
+      </nav>
+    );
+  }
+}
 
 export default Nav;
