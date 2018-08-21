f60625aada9582cd471c530eec798eaffa8e3bb1
Jay Hwang <jayhwang0121@gmail.com>
2018-08-20 20:29:34 -0700

    fix(BlogHeader): make payout decimal round to nearest 10th

--- src/components/blogs/BlogHeader.js
+++ src/components/blogs/BlogHeader.js
@@ -1,29 +1,32 @@
 import React from 'react';
 
-const BlogHeader = ({ blog }) => (
-  <header className="blog-header">
-    <section className="about-author">
-      <div className="author-profile-pic">
-        {/* { <img src={ blog.author.profile_pic_url } /> } */}
-      </div>
-      <div className="info">
-        <h3>
-          { blog.author }
-        </h3>
+const BlogHeader = ({ blog }) => {
+  const payoutValue = parseInt(blog.pendingPayoutValue).toFixed(2);
+  return (
+    <header className="blog-header">
+      <section className="about-author">
+        <div className="author-profile-pic">
+          {/* { <img src={ blog.author.profile_pic_url } /> } */}
+        </div>
+        <div className="info">
+          <h3>
+            { blog.author }
+          </h3>
+          <p>
+            { blog.created }
+          </p>
+        </div>
+      </section>
+      <section>
+        <h2>
+          ${ payoutValue }
+        </h2>
         <p>
-          { blog.created }
+          {/* { blog.readTime } */}
         </p>
-      </div>
-    </section>
-    <section>
-      <h2>
-        ${ blog.curator_payout_value }
-      </h2>
-      <p>
-        {/* { blog.readTime } */}
-      </p>
-    </section>
-  </header>
-);
+      </section>
+    </header>
+  );
+}
 
 export default BlogHeader;
