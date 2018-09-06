9db5bd616456e1ff290d385d40e0ad6f9c784376
Jay Ithiel <jay.ithiel@gmail.com>
2018-09-06 00:30:26 -0700

    Create Account skeleton

---
+++ src/components/users/account/Account.js
@@ -0,0 +1,22 @@
+// React
+import React from 'react';
+
+// Apollo / GraphQL
+import { Query } from 'react-apollo';
+
+// HOCs
+import Layout from '../../HOCs/Layout';
+
+// Components
+
+class Account extends React.Component {
+  render() {
+    return (
+      <Layout>
+        Account
+      </Layout>
+    );
+  }
+}
+
+export default Account;
