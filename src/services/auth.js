b042c5c57cddd1380eb414484a0ce1ffd3285718
Jay Ithiel <jay.ithiel@gmail.com>
2018-10-10 01:22:23 -0700

    Refactor auth service

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,13 +1,43 @@
 import steemConnect from './steemConnect';
 
-const SteemAuth = {
+const AuthService = {
+  login: authResponse => {
+    const steemUserData = _parseSteemUserParams(authResponse);
+    _cache(steemUserData);
+  },
 
   logout: () => {
     steemConnect.revokeToken();
-    localStorage.removeItem('access_token');
-    localStorage.removeItem('username');
-    localStorage.removeItem('expires_in');
+    _clearCache();
   },
 }
 
-export default SteemAuth;
+export default AuthService;
+
+
+function _cache(userData) {
+  userData.forEach(metaData => {
+    const [key, value] = _parseSteemUserData(metaData);
+    localStorage.setItem(key, value);
+  });
+}
+
+function _clearCache() {
+  localStorage.removeItem('access_token');
+  localStorage.removeItem('username');
+  localStorage.removeItem('expires_in');
+}
+
+function _parseSteemUserData(userData) {
+  return userData.split('=');
+}
+
+function _parseSteemUserParams(steemUserParams) {
+  let [steemToken, expiresIn, username] = steemUserParams.split('&');
+  steemToken = steemToken.slice(1); // Remove '&' prefix
+  return [
+    steemToken,
+    expiresIn,
+    username,
+  ];
+}
