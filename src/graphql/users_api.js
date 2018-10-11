de2338ba2bed4657eb4572ff57a6fc350d5e0f16
jay-ithiel <jay.ithiel@gmail.com>
2018-10-11 16:10:36 -0700

    Add getProfileInformation query

--- src/graphql/users_api.js
+++ src/graphql/users_api.js
@@ -1,5 +1,26 @@
 import gql from 'graphql-tag';
 
+export const getProfileInformation = gql`
+  query getProfileInformation($name: String!) {
+    getProfileInformation(name: $name) {
+      id
+      name
+      votingPower
+      location
+      realLifeName
+      introBlurb
+      reputationScore
+      profileImageUrl
+      posts {
+        id
+      }
+      replies {
+        id
+      }
+    }
+  }
+`;
+
 export const createUser = user => (
   gql``
 );
