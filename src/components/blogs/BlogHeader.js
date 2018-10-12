5843d5db1acb57af29012588acb72522bd6057bd
Jay Ithiel <jay.ithiel@gmail.com>
2018-10-11 20:44:46 -0700

    Beginning connecting steemUser

--- src/components/blogs/BlogHeader.js
+++ src/components/blogs/BlogHeader.js
@@ -1,31 +1,48 @@
+// React
 import React from 'react';
+
+// Apollo / GraphQL
+import { Query } from 'react-apollo';
+import { getProfileInformation } from '../../graphql/users_api';
+import { GqlError, Loading } from '../shared';
 
 const BlogHeader = ({ blog }) => {
   const payoutValue = parseInt(blog.pendingPayoutValue, 10).toFixed(2);
+  const username = 'adetorrent';//localStorage.getItem('username');
   return (
-    <header className="blog-header">
-      <section className="about-author">
-        <div className="author-profile-pic">
-          {/* { <img src={ blog.author.profile_pic_url } /> } */}
-        </div>
-        <div className="info">
-          <h3>
-            { blog.author }
-          </h3>
-          <p>
-            { blog.created }
-          </p>
-        </div>
-      </section>
-      <section>
-        <h2>
-          ${ payoutValue }
-        </h2>
-        <p>
-          {/* { blog.readTime } */}
-        </p>
-      </section>
-    </header>
+    <Query query={ getProfileInformation } variables={{ name: username }}>
+      {
+        ({ loading, error, data }) => {
+          if (loading) return <Loading/>;
+          if (error) return <GqlError error={ error }/>
+          const steemUser = data.getProfileInformation;
+          if (!steemUser) return <div>You have no profile info to show yet!</div>
+          return (
+            <header className="blog-header">
+              <section className="about-author">
+                <div className="author-profile-pic" style={{ backgroundImage: `url(${steemUser.profileImageUrl})`}}></div>
+                <div className="info">
+                  <h3>
+                    {blog.author}
+                  </h3>
+                  <p>
+                    {blog.created}
+                  </p>
+                </div>
+              </section>
+              <section>
+                <h2>
+                  ${payoutValue}
+                </h2>
+                <p>
+                  {/* { blog.readTime } */}
+                </p>
+              </section>
+            </header>
+          );
+        }
+      }
+    </Query>
   );
 }
 
