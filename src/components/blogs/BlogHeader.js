85a682df5da45383ed7ea10967b94a8dfeceb0af
Jay Ithiel <jay.ithiel@gmail.com>
2018-10-17 18:18:25 -0700

    remove unnecessary query

--- src/components/blogs/BlogHeader.js
+++ src/components/blogs/BlogHeader.js
@@ -1,48 +1,32 @@
 // React
 import React from 'react';
 
-// Apollo / GraphQL
-import { Query } from 'react-apollo';
-import { getProfileInformation } from '../../graphql/users_api';
-import { GqlError, Loading } from '../shared';
-
 const BlogHeader = ({ blog }) => {
   const payoutValue = parseInt(blog.pendingPayoutValue, 10).toFixed(2);
-  const username = 'adetorrent';//localStorage.getItem('username');
+  const { author } = blog;
+  const profileImageUrl = author.profileImageUrl || 'https://res.cloudinary.com/ddgtwtbre/image/upload/v1539314358/avatar_nt5vnh.jpg';
   return (
-    <Query query={ getProfileInformation } variables={{ name: username }}>
-      {
-        ({ loading, error, data }) => {
-          if (loading) return <Loading/>;
-          if (error) return <GqlError error={ error }/>
-          const steemUser = data.getProfileInformation;
-          if (!steemUser) return <div>You have no profile info to show yet!</div>
-          return (
-            <header className="blog-header">
-              <section className="about-author">
-                <div className="author-profile-pic" style={{ backgroundImage: `url(${steemUser.profileImageUrl})`}}></div>
-                <div className="info">
-                  <h3>
-                    {blog.author}
-                  </h3>
-                  <p>
-                    {blog.created}
-                  </p>
-                </div>
-              </section>
-              <section>
-                <h2>
-                  ${payoutValue}
-                </h2>
-                <p>
-                  {/* { blog.readTime } */}
-                </p>
-              </section>
-            </header>
-          );
-        }
-      }
-    </Query>
+    <header className="blog-header">
+      <section className="about-author">
+        <div className="author-profile-pic" style={{ backgroundImage: `url(${profileImageUrl})`}}></div>
+        <div className="info">
+          <h3>
+            {author.name}
+          </h3>
+          <p>
+            {blog.created}
+          </p>
+        </div>
+      </section>
+      <section>
+        <h2>
+          ${payoutValue}
+        </h2>
+        <p>
+          {/* { blog.readTime } */}
+        </p>
+      </section>
+    </header>
   );
 }
 
