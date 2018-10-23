20d5353f964ec35a33e167f6295b9463c6a29ec2
Jay Ithiel <jay.ithiel@gmail.com>
2018-10-22 20:58:56 -0700

    Refactor into functional component

--- src/components/blogs/BlogRoutes.jsx
+++ src/components/blogs/BlogRoutes.jsx
@@ -1,12 +1,5 @@
 import React from 'react';
-import { Link, Redirect, Route, Switch } from 'react-router-dom';
-import { Query } from 'react-apollo';
-
-import { fetchBlogs } from '../../graphql/blogs_api';
-
-// Components
-import Layout from '../../HOCs/Layout';
-import { GqlError, Loading } from '../shared';
+import { Route, Switch } from 'react-router-dom';
 
 import Blog from './Blog';
 import Blogs from './Blogs';
@@ -16,33 +9,19 @@ import NewsBlogs from './NewsBlogs';
 import StoryBlogs from './StoryBlogs';
 import VideoBlogs from './VideoBlogs';
 
+/** TODO: Add AuthenticatedRoute component */
 
-class BlogRoutes extends React.Component {  
-  render() {
-    return (
-      <Layout>
-        <Query query={ fetchBlogs }>
-          {({ data, error, loading }) => {
-            if (loading) return <Loading />
-            if (error) return <GqlError error={ error } />
-            const blogs = data.getAllPosts;
-            return (
-              <Switch>
-                <Route exact path="/" component= { Blogs } />
-                <Route exact path="/blogs/feed" component={ FeedBlogs } />
-                <Route exact path="/blogs/new" component={ BlogForm } />
-                <Route exact path="/blogs/news" component={ NewsBlogs } />
-                <Route exact path="/blogs/story" component={StoryBlogs} />
-                <Route exact path="/blogs/video" component={VideoBlogs} />
-                <Route exact path="/blogs/:id" component={ Blog } />
-                <Route exact path="/blogs/edit/:id" component={ BlogForm } />
-              </Switch>
-            );
-          }}
-        </Query>
-      </Layout>
-    );
-  }
-}
+const BlogRoutes = () => (
+  <Switch>
+    <Route exact path="/" component= { Blogs } />
+    <Route exact path="/blogs/feed" component={ FeedBlogs } />
+    <Route exact path="/blogs/new" component={ BlogForm } />
+    <Route exact path="/blogs/news" component={ NewsBlogs } />
+    <Route exact path="/blogs/story" component={StoryBlogs} />
+    <Route exact path="/blogs/video" component={VideoBlogs} />
+    <Route exact path="/blogs/:id" component={ Blog } />
+    <Route exact path="/blogs/edit/:id" component={ BlogForm } />
+  </Switch>
+);
 
 export default BlogRoutes;
