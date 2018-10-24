dd49a1f3b2be9a6a9bd298c5ca2c8725b353c5ba
Jay Ithiel <jay.ithiel@gmail.com>
2018-10-23 19:36:33 -0700

    Begin creating replies

---
+++ src/components/replies/Replies.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Query } from 'react-apollo';
+
+import { GET_ALL_POST_REPLIES } from '../../graphql/replies_api';
+
+// Components
+import Layout from '../../HOCs/Layout';
+import { GqlError, Loading } from '../shared';
+
+const Replies = ({ postId }) => (
+  <Layout>
+    <Query query={ GET_ALL_POST_REPLIES } variables={{ postId }}>
+      {({ data, error, loading }) => {
+        if (loading) return <Loading />;
+        if (error) return <GqlError error={ error } />;
+        const replies = data.getAllPostReplies;
+        return (
+          <section className="replies">
+            <div className="replies--content">
+              { replies.map(reply => <Reply reply={reply} key={reply.id} />) }
+            </div>
+          </section>
+        );
+      }}
+    </Query>
+  </Layout>
+);
+
+export default Replies;
