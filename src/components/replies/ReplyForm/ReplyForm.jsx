58f120f1f21f9930ec388a7e06a11baa94178dcb
Jay Ithiel <jay.ithiel@gmail.com>
2018-10-24 19:36:50 -0700

    Create ReplyForm

---
+++ src/components/replies/ReplyForm/ReplyForm.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { compose, graphql } from 'react-apollo';
+
+import { BROADCAST_REPLY, GET_ALL_POST_REPLIES } from '../../../graphql/replies_api';
+
+class ReplyForm extends React.Component {
+  state = {
+    reply: {
+      body: '',
+      postAuthor: '',
+      postPermLink: '',
+    },
+    errors: { },
+  };
+
+  componentDidMount() {
+    const post = this.props.blog;
+    this.setState({
+      reply: {
+        body: '',
+        postAuthor: post.author.name,
+        postPermLink: post.permLink,
+      },
+    });
+  }
+  
+  render() {
+    return (
+      <div className="reply-form-container">
+        <form>
+          <textarea placeholder="Write your comment here.."></textarea>
+          <div className="reply-form--actions">
+            <button type="button">Cancel</button>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+    const { reply } = this.state;
+    this.props.broadcastReply(reply);
+  }
+}
+
+export default compose(
+  graphql(BROADCAST_REPLY, {
+    props: ({ mutate }) => ({
+      broadcastReply: reply => {
+        return mutate({
+          variables: { reply },
+          refetchQueries: [{ query: GET_ALL_POST_REPLIES }],
+          update: (store, { data: { broadcastReply } }) => {
+            if (!store.data.data.ROOT_QUERY) return;
+            const { getAllPostReplies } = store.readQuery({ query: GET_ALL_POST_REPLIES });
+            getAllPostReplies.push(broadcastReply);
+            store.writeQuery({ query: GET_ALL_POST_REPLIES, data: { getAllPostReplies } });
+          },
+        });
+      },
+    }),
+  })
+)(ReplyForm);
