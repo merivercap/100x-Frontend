6a26b304eb885c0dfa30a05ea9c7ae4c8c8c3b7a
Jay Ithiel <jay.ithiel@gmail.com>
2018-10-30 10:20:00 -0700

    Minor CSS styling changes

--- src/components/blogs/BlogHeader.jsx
+++ src/components/blogs/BlogHeader.jsx
@@ -4,33 +4,27 @@ import React from 'react';
 const BlogHeader = ({ blog }) => {
   const payoutValue = parseInt(blog.pendingPayoutValue, 10).toFixed(2);
   const { author } = blog;
-  if (!author) {
-    return <div>Blog has no author</div>
-  }
-  const profileImageUrl = author && author.profileImageUrl ? author.profileImageUrl : 'https://res.cloudinary.com/ddgtwtbre/image/upload/v1539314358/avatar_nt5vnh.jpg';
+  if (!author) return <div>Blog has no author</div>;
+  const profileImageUrl = author && author.profileImageUrl ? author.profileImageUrl : 'https://res.cloudinary.com/ddgtwtbre/image/upload/v1482131647/person-solid_telh7f.png';
+  debugger;
   return (
     <header className="blog-header">
       <section className="about-author">
-        <div className="author-profile-pic" style={{ backgroundImage: `url(${profileImageUrl})`}}></div>
+        <div className="author-profile-pic" style={{ backgroundImage: `url(${profileImageUrl})` }}></div>
         <div className="info">
-          <h3>
-            {author.name}
-          </h3>
-          <p>
-            {blog.created}
-          </p>
+          <h3>{author.name}</h3>
+          <p>{blog.createdAt}</p>
         </div>
       </section>
       <section>
-        <h2>
-          ${payoutValue}
-        </h2>
-        <p>
-          {/* { blog.readTime } */}
-        </p>
+        <h2>${payoutValue}</h2>
+        <p>{/* { blog.readTime } */}</p>
       </section>
     </header>
   );
 }
 
 export default BlogHeader;
+
+
+function parseTo
