2debd186c97280b8e1a515b39754778b34d8aff6
Jay Ithiel <jay.ithiel@gmail.com>
2018-10-31 16:38:41 -0700

    Create active state for blogtypebar

--- src/components/blogs/Forms/BlogTypeBar/BlogTypeBar.jsx
+++ src/components/blogs/Forms/BlogTypeBar/BlogTypeBar.jsx
@@ -3,16 +3,19 @@ import React from 'react';
 // Utils
 import { BLOG_TYPES } from '../../../../utils/blogTypes';
 
-const BlogTypeBar = ({ handleTypeChange }) => (
+const BlogTypeBar = ({ blogType, handleTypeChange }) => (
   <nav className="blog-type-bar--container">
     <div className="blog-type--item" onClick={ handleTypeChange(BLOG_TYPES.story) }>
-      Story
+      <span>Story</span>
+      { blogType === BLOG_TYPES.story && <div className="active-bar"></div> }
     </div>
     <div className="blog-type--item" onClick={ handleTypeChange(BLOG_TYPES.news) }>
-      News
+      <span>News</span>
+      { blogType === BLOG_TYPES.news && <div className="active-bar"></div> }
     </div>
     <div className="blog-type--item" onClick={ handleTypeChange(BLOG_TYPES.video) }>
-      Video
+      <span>Video</span>
+      { blogType === BLOG_TYPES.video && <div className="active-bar"></div> }
     </div>
   </nav>
 );
