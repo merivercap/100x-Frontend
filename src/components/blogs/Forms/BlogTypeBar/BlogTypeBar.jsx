765113f9f0a7119242358c028455b1eb667c2fd5
Jay Ithiel <jay.ithiel@gmail.com>
2018-10-31 16:25:54 -0700

    reorder

--- src/components/blogs/Forms/BlogTypeBar/BlogTypeBar.jsx
+++ src/components/blogs/Forms/BlogTypeBar/BlogTypeBar.jsx
@@ -5,11 +5,11 @@ import { BLOG_TYPES } from '../../../../utils/blogTypes';
 
 const BlogTypeBar = ({ handleTypeChange }) => (
   <nav className="blog-type-bar--container">
-    <div className="blog-type--item" onClick={ handleTypeChange(BLOG_TYPES.news) }>
-      News
-    </div>
     <div className="blog-type--item" onClick={ handleTypeChange(BLOG_TYPES.story) }>
       Story
+    </div>
+    <div className="blog-type--item" onClick={ handleTypeChange(BLOG_TYPES.news) }>
+      News
     </div>
     <div className="blog-type--item" onClick={ handleTypeChange(BLOG_TYPES.video) }>
       Video
