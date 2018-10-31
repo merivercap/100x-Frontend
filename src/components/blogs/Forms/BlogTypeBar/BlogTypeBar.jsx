f19b3247f15defe09908d32a735a8665a850b730
Jay Ithiel <jay.ithiel@gmail.com>
2018-10-31 13:44:45 -0700

    Create Blog type selector bar

---
+++ src/components/blogs/Forms/BlogTypeBar/BlogTypeBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// Utils
+import { BLOG_TYPES } from '../../../../utils/blogTypes';
+
+const BlogTypeBar = ({ handleTypeChange }) => (
+  <nav className="blog-type-bar--container">
+    <div className="blog-type--item" onClick={ handleTypeChange(BLOG_TYPES.news) }>
+      News
+    </div>
+    <div className="blog-type--item" onClick={ handleTypeChange(BLOG_TYPES.story) }>
+      Story
+    </div>
+    <div className="blog-type--item" onClick={ handleTypeChange(BLOG_TYPES.video) }>
+      Video
+    </div>
+  </nav>
+);
+
+export default BlogTypeBar;
