6e002e2e135dd9fda48e8d1974c94c38f3b22dfe
Jay Hwang <jayhwang0121@gmail.com>
2018-11-01 12:51:39 -0700

    Remove Layout wrapper

--- src/components/replies/Replies.jsx
+++ src/components/replies/Replies.jsx
@@ -10,24 +10,22 @@ import Reply from './Reply';
 import ReplyForm from './ReplyForm';
 
 const Replies = ({ post }) => (
-  <Layout>
-    <Query query={ GET_ALL_POST_REPLIES } variables={{ postId: post.id }}>
-      {({ data, error, loading }) => {
-        if (loading) return <Loading />;
-        if (error) return <GqlError error={ error } />;
-        const replies = data.getAllPostReplies;
-        return (
-          <section className="replies">
-            <div className="replies--content">
-              <h1>Replies</h1>
-              <ReplyForm post={post} />
-              { replies.map(reply => <Reply reply={reply} key={reply.id} />) }
-            </div>
-          </section>
-        );
-      }}
-    </Query>
-  </Layout>
+  <Query query={ GET_ALL_POST_REPLIES } variables={{ postId: post.id }}>
+    {({ data, error, loading }) => {
+      if (loading) return <Loading />;
+      if (error) return <GqlError error={ error } />;
+      const replies = data.getAllPostReplies;
+      return (
+        <section className="replies">
+          <div className="replies--content">
+            <ReplyForm post={post} />
+            <h1>Replies</h1>
+            { replies.map(reply => <Reply reply={reply} key={reply.id} />) }
+          </div>
+        </section>
+      );
+    }}
+  </Query>
 );
 
 export default Replies;
