b1bde6617b96e23b212d060f5424967d0913ca9f
Jay Ithiel <jay.ithiel@gmail.com>
2018-11-05 15:05:56 -0800

    Consolidate nav filter components'

--- src/components/nav/NavFilter/NavFilter.jsx
+++ src/components/nav/NavFilter/NavFilter.jsx
@@ -1,5 +1,6 @@
 // React
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // Components
 import NavBlogFilter from './NavBlogFilter';
@@ -8,7 +9,23 @@ export default class NavFilter extends React.Component {
   render() {
     return (
       <div className="nav--filter-container">
-        <NavBlogFilter />
+        <div className="nav--filter">
+          <Link to="/" className="nav-filter--item">
+            All Blogs
+        </Link>
+          <Link to="/blogs/feed" className="nav-filter--item">
+            Home
+        </Link>
+          <Link to="/blogs/story" className="nav-filter--item">
+            Blog
+        </Link>
+          <Link to="/blogs/news" className="nav-filter--item">
+            News
+        </Link>
+          <Link to="/blogs/video" className="nav-filter--item">
+            Video
+        </Link>
+        </div>
       </div>
     );
   }
