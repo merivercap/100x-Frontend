f1167852c1560ab3c9e1141ff9646fec29150df3
Jay Ithiel <jay.ithiel@gmail.com>
2018-11-05 17:29:27 -0800

    Refactor Navfilter component to be responsive

--- src/components/nav/NavFilter/NavFilter.jsx
+++ src/components/nav/NavFilter/NavFilter.jsx
@@ -1,37 +1,58 @@
 // React
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 import { AppConsumer, ScreenType } from '../../shared/ScreenType';
 
-const NavFilter = () => (
-  <div className="nav--filter-container">
-    <AppConsumer>
-      {({ screenType }) => {
-        console.log('screenType: ', screenType);
-        if (screenType < ScreenType.TABLET) {
-          return <NavFilterMobile />;
-        } else {
-          return <NavFilterDesktop />;
-        }
-      }}
-    </AppConsumer>
-  </div>
-);
+const FeedType = {
+  ALL_BLOGS: 'All Blogs',
+  BLOG: 'Blog',
+  HOME: 'Home',
+  NEWS: 'News',
+  VIDEO: 'Video',
+};
+
+class NavFilter extends React.Component {
+  state = {
+    feedType: FeedType.ALL_BLOGS,
+  };
+  
+  render() {
+    const { feedType } = this.state;
+    return (
+      <div className="nav--filter-container">
+        <AppConsumer>
+          {({ screenType }) => {
+            console.log('screenType: ', screenType);
+            if (screenType < ScreenType.TABLET) {
+              return <NavFilterMobile feedType={feedType} />;
+            } else {
+              return <NavFilters />;
+            }
+          }}
+        </AppConsumer>
+      </div>
+    );
+  };
+
+  updateFeedType = updatedFeedType => {
+    this.setState({ feedType: updatedFeedType });  
+  };
+}
 
 export default NavFilter;
 
 
 /** Helper Components */
 
-const NavFilterMobile = () => (
-  <div>
-    Mobile
+const NavFilterMobile = ({ feedType }) => (
+  <div className="nav--filter-container mobile">
+    <button>{ feedType }</button>
+    <NavFilters />
   </div>
 );
 
-const NavFilterDesktop = () => (
-  <div>
+const NavFilters = () => (
+  <div className="nav--filter-container">
     <div className="nav--filter">
       <Link to="/" className="nav-filter--item">
         All Blogs
