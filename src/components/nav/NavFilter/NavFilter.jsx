0b7d1ecc26e832752b77ea40298c8eabe8472881
Jay Ithiel <jay.ithiel@gmail.com>
2018-11-05 16:33:02 -0800

    integrate ScreenType compnoent

--- src/components/nav/NavFilter/NavFilter.jsx
+++ src/components/nav/NavFilter/NavFilter.jsx
@@ -2,28 +2,52 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default class NavFilter extends React.Component {
-  render() {
-    return (
-      <div className="nav--filter-container">
-        <div className="nav--filter">
-          <Link to="/" className="nav-filter--item">
-            All Blogs
-          </Link>
-          <Link to="/blogs/feed" className="nav-filter--item">
-            Home
-          </Link>
-          <Link to="/blogs/story" className="nav-filter--item">
-            Blog
-          </Link>
-          <Link to="/blogs/news" className="nav-filter--item">
-            News
-          </Link>
-          <Link to="/blogs/video" className="nav-filter--item">
-            Video
-          </Link>
-        </div>
-      </div>
-    );
-  }
-}
+import { AppConsumer, ScreenType } from '../../shared/ScreenType';
+
+const NavFilter = () => (
+  <div className="nav--filter-container">
+    <AppConsumer>
+      {({ screenType }) => {
+        console.log('screenType: ', screenType);
+        if (screenType < ScreenType.TABLET) {
+          return <NavFilterMobile />;
+        } else {
+          return <NavFilterDesktop />;
+        }
+      }}
+    </AppConsumer>
+  </div>
+);
+
+export default NavFilter;
+
+
+/** Helper Components */
+
+const NavFilterMobile = () => (
+  <div>
+    Mobile
+  </div>
+);
+
+const NavFilterDesktop = () => (
+  <div>
+    <div className="nav--filter">
+      <Link to="/" className="nav-filter--item">
+        All Blogs
+      </Link>
+      <Link to="/blogs/feed" className="nav-filter--item">
+        Home
+      </Link>
+      <Link to="/blogs/story" className="nav-filter--item">
+        Blog
+      </Link>
+      <Link to="/blogs/news" className="nav-filter--item">
+        News
+      </Link>
+      <Link to="/blogs/video" className="nav-filter--item">
+        Video
+      </Link>
+    </div>
+  </div>
+);
