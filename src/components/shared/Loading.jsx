9f242002c8c3ce08d0a05aa322cb223ce074a66b
Jay Ithiel <jay.ithiel@gmail.com>
2018-11-05 16:32:32 -0800

    Fix syntax error

--- src/components/shared/Loading.jsx
+++ src/components/shared/Loading.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Loading = () => (
   <div className="gql-loading">
-    <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+    <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
     <h1>Loading</h1>
   </div>
 );
